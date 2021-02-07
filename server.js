97db391e97685ae62ffff07f30e0abd82eed63e6
RahulVPol <rahul.pol@cimpress.com>
2021-02-08 02:52:48 +0530

    commit-6: Setup CORS policy for Origin, Method & Headers

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ app.set("view engine", "pug");
 
 // app.use(bodyParser.urlencoded()) // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, HEAD"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/v1/feed", feedRoutes);
