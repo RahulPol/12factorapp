c0fccb1d28ff4673ee7b4dd23bec9538c4d4d3e7
RahulVPol <rahul.pol@cimpress.com>
2021-02-08 01:30:18 +0530

    commit-3: Basic swagger setup

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const jsyaml = require("js-yaml");
+const path = require("path");
 
+const spec = fs.readFileSync(path.join(__dirname, "swagger.yml"), "utf-8");
+const swaggerDocument = jsyaml.load(spec);
 const feedRoutes = require("./routes/feedRoutes");
 
 const app = express();
@@ -8,7 +14,9 @@ const app = express();
 // app.use(bodyParser.urlencoded()) // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
-app.use("/feed", feedRoutes);
+app.use("/v1/feed", feedRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(8080, () => {
   console.log("Server started!");
