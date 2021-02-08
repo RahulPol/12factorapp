9177e7afbbbc2e8eff9ba261c18439b9ece9b1cf
RahulVPol <rahul.pol@cimpress.com>
2021-02-08 17:16:53 +0530

    commit-8: updated get posts - added few extra fields - added logic for getting base url

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,9 +1,22 @@
+const path = require("path");
+
 const { HTTP_OK, HTTP_CREATED } = require("../common/constants");
+const { getBaseURL } = require("../common/utilities");
 
 exports.getPosts = (req, res, next) => {
   // .json will set "Content-Type":"application/json" response header
   res.status(HTTP_OK).json({
-    posts: [{ title: "First post", content: "This is first post!" }],
+    posts: [
+      {
+        title: "First post",
+        content: "This is first post!",
+        imageUrl: getBaseURL(req) + "/images/avatar-female.jpg",
+        creator: {
+          name: "Rahul Pol",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
