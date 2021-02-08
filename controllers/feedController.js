68ec3c1183572851ff31a9bbde9880c4b0654575
RahulVPol <rahul.pol@cimpress.com>
2021-02-08 19:52:43 +0530

    commit-10: Added validation

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,6 +1,11 @@
 const path = require("path");
+const { validationResult } = require("express-validator/check");
 
-const { HTTP_OK, HTTP_CREATED } = require("../common/constants");
+const {
+  HTTP_OK,
+  HTTP_CREATED,
+  HTTP_VALIDATION_FAILED,
+} = require("../common/constants");
 const { getBaseURL } = require("../common/utilities");
 
 exports.getPosts = (req, res, next) => {
@@ -21,12 +26,29 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(HTTP_VALIDATION_FAILED).json({
+      message: " Validation failed. Entered data is incorrect! ",
+      errors: errors.array(),
+    });
+  }
+
   const title = req.body.title;
   const content = req.body.content;
 
   //TODO: create a post in Db
   res.status(HTTP_CREATED).json({
     message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title, content },
+    post: {
+      _id: new Date().toISOString(),
+      title,
+      content,
+      imageUrl: getBaseURL(req) + "/images/avatar-female.jpg",
+      creator: {
+        name: "Rahul Pol",
+      },
+      createdAt: new Date(),
+    },
   });
 };
