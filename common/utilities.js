52d10c280907f3be6caa77c0a296a1dee61ec265
RahulVPol <rahul.pol@cimpress.com>
2021-02-11 16:18:47 +0530

    commit-1: Setting up backing services Added db utility Added database env variables Updated content of principle in index

--- common/utilities.js
+++ common/utilities.js
@@ -1,3 +1,5 @@
+const path = require("path");
+const fs = require("fs");
 const { PORT } = require("./constants");
 
 exports.getBaseURL = (req) => {
@@ -5,6 +7,20 @@ exports.getBaseURL = (req) => {
     req.protocol +
     "://" +
     req.host +
-    (process.env.PORT == 80 || process.env.PORT == 443 ? "" : ":" + PORT)
+    (process.env.PORT == 80 || process.env.PORT == 443
+      ? ""
+      : ":" + process.env.PORT)
   );
+};
+
+exports.clearImage = (filePath) => {
+  filePath = path.join(
+    __dirname,
+    "..",
+    "public\\images",
+    filePath.substr(filePath.lastIndexOf("\\") + 1, filePath.length)
+  );
+  fs.unlink(filePath, (err) => {
+    console.log(err);
+  });
 };
---
+++ models/dbUtil.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const constants = require("../common/constants");
+const MongoUtil = require("./mongoUtil");
+
+const connectToMongodb = (databaseUri) => {
+  // connect to mongodb
+  return mongoose.connect(databaseUri);
+};
+
+const connectToMySql = (databaseUri) => {
+  // connect to mysql
+  // this project is for demo purpose so returning reject for now.
+  return Promise.reject("No config present for mysql, yet!");
+};
+
+exports.connectDB = (database, databaseUri) => {
+  switch (database) {
+    case "mongodb":
+      return connectToMongodb(databaseUri);
+    case "mySql":
+      return connectToMySql(databaseUri);
+    default:
+      return Promise.reject("Invalid database");
+  }
+};
+
+exports.getDbInstance = (database) => {
+  let error;
+  let IDatabase;
+  switch (database) {
+    case "mongodb":
+      IDatabase = new MongoUtil();
+      return IDatabase;
+    case "mySql":
+      error = new Error("No config present for mysql, yet!");
+      error.statusCode = constants.HTTP_INTERNAL_SERVER_ERROR;
+      throw error;
+    default:
+      error = new Error("Invalid database");
+      error.statusCode = constants.HTTP_INTERNAL_SERVER_ERROR;
+      throw error;
+  }
+};
